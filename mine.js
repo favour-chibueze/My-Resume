fd368378872bc0d863d415602b6503d6b31ed248
Favour Chibueze <46398983+favour-chibueze@users.noreply.github.com>
2019-09-04 17:09:07 +0100

    Added form validation

---
+++ mine.js
@@ -0,0 +1,20 @@
+function validate()
+if( document.Form.email.value == "" )
+{
+   alert( "Please provide your Email!" );
+   document.Form.email.focus() ;
+   return false;
+}
+
+let emailID = document.Form.EMail.value;
+atpos = emailID.indexOf("@");
+dotpos = emailID.lastIndexOf(".");
+
+if (atpos < 1 || (dotpos - atpos < 2 )) 
+{
+   alert("Please enter correct email ID")
+   document.Form.email.focus() ;
+   return false;
+}
+return( true );
+}
